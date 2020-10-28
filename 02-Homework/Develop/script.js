26733505783e13a30b430dc3a5a9b7f7e6577e8c
Daniel Reza <daniel.reza1007@gmail.com>
2020-10-27 21:44:03 -0500

    updating project

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -4,10 +4,33 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+    function generatePassword() {
+
+    var passwordLength = [];
+
+    if (generateBtn) {
+      window.alert("Please answer the following questions in order to generate your password.");
+    }
+
+      var length=prompt("How long would you like your password to be? (Please select a number between 8-128)");
+
+        if (length < 8 || length > 128 || length == null) {
+          alert("You must enter a valid number. Please try again."); 
+        } else { 
+            alert("Your password will be " + length + " characters long.");
+            passwordLength.push(Number(length));
+        }
+
+        
+
+    }
+
+
+
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
