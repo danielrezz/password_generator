40a18e40c8653bdd4aa0d6b028b7cc5bb2399f4c
Daniel Reza <daniel.reza1007@gmail.com>
2020-10-28 23:04:50 -0500

    adding character arrays

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -7,6 +7,10 @@ function writePassword() {
     function generatePassword() {
 
     var passwordLength = [];
+    var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var numChars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "-", "_"];
 
     if (generateBtn) {
       window.alert("Please answer the following questions in order to generate your password.");
