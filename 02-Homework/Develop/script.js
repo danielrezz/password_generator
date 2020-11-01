91006f7b8144cb288ed9467557ab9034118e6e7e
Daniel Reza <daniel.reza1007@gmail.com>
2020-10-31 19:13:14 -0500

    updating README

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -9,6 +9,8 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
+// Strings of characters than can be chosen by user
   var lowerCase = "abcdefghijklmnopqrstuvwxyz";
     var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var numChars = "0123456789";
@@ -16,23 +18,6 @@ function writePassword() {
 
     function generatePassword() {
 
-// This is how long the password will be (chosen by user)
-    
-
-// Character arrays that will be used based on user selection
-    // var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    // var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    // var numChars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    // var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "-", "_"];
-
-// This array will store the user's selected characters
-
-// When user clicks Generate Password button, they are notified of the steps they must take
-    // if (generateBtn) {
-    //   window.alert("Please answer the following questions in order to generate your password.");
-    // }
-
-// If user does not give an answer or provides invalid answer, they are notified and must start over
     var passwordLength = prompt("How long would you like your password to be? (Please select a number between 8-128)");
 
       if ((passwordLength > 8) && (passwordLength < 128)) {
@@ -50,24 +35,15 @@ function writePassword() {
         console.log(userChars);
       }
 
-// If user confirms, upper case array is pushed into userChars array
-    
-
       if (upper) {
         userChars += upperCase;
         console.log(userChars);
       }
 
-// If user confirms, numbers array is pushed into userChars array
-    
-
       if (numbers) {
         userChars += numChars;
         console.log(userChars);
       }
-
-// If user confirms, special characters array is pushed into userChars array
-    
 
       if (special) {
         userChars += specialChars;
@@ -82,8 +58,6 @@ function writePassword() {
           alert("Password length is incorrect");
           
       }
-
-// If user confirms, lower case array is pushed into userChars array
     
       return newPassword;
     }
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# password_generator
+<h1>
+Password Generator
+</h1>
+
+<h2>
+Summary
+</h2>
+
+<p>
+This is a generator made using Javascript that creates a randomized password based on the user's selection.
+</p>
+
